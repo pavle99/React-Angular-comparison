4d851e83747fb2e4fe147276f2b8e3db3a073736
Pavle Cvejovic <pavle.cvejovic@qcerris.com>
2023-08-15 13:22:26 +0200

    component segregation in angular

--- angular-example/src/app/note/note.component.ts
+++ angular-example/src/app/note/note.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { INote } from 'src/types/note';
 
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
-  styleUrls: ['./note.component.css']
+  styleUrls: ['./note.component.css'],
 })
 export class NoteComponent {
+  @Input() note!: INote;
 
+  @Input() toggleCompleteNote!: (id: number) => void;
+
+  @Input() deleteNote!: (id: number) => void;
 }
